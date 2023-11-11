684a1e132a9902ab8a7197ec78e105ce298bd1e0
ThngBlvck <thonghhpc05659@fpt.edu.vn>
2023-11-11 22:07:28 +0700

    pdo category - add- list

--- admin/category/list.php
+++ admin/category/list.php
@@ -41,109 +41,26 @@ include "components/sidebar.php";
                                         <thead>
                                         <tr>
                                             <th>ID</th>
-                                            <th>User</th>
-                                            <th>Date</th>
-                                            <th>Status</th>
-                                            <th>Reason</th>
-                                            <th>1234567</th>
+                                            <th>Name</th>
+                                            <th></th>
+
                                         </tr>
                                         </thead>
                                         <tbody>
+                                        <?php
+
+                                        echo '
                                         <tr>
-                                            <td>183</td>
-                                            <td>John Doe</td>
-                                            <td>11-7-2014</td>
-                                            <td><span class="tag tag-success">Approved</span></td>
-                                            <td>sdfghjkl;ẻtyuio</td>
-                                            <td>
-                                                <a href="" class="btn btn-primary">Sửa</a>
-                                                <a href="" class="btn btn-danger">Xóa</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>219</td>
-                                            <td>Alexander Pierce</td>
-                                            <td>11-7-2014</td>
-                                            <td><span class="tag tag-warning">Pending</span></td>
-                                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.
-                                            </td>
-                                            <td>
-                                                <a href="" class="btn btn-primary">Sửa</a>
-                                                <a href="" class="btn btn-danger">Xóa</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>657</td>
-                                            <td>Bob Doe</td>
-                                            <td>11-7-2014</td>
-                                            <td><span class="tag tag-primary">Approved</span></td>
-                                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.
-                                            </td>
-                                            <td>
-                                                <a href="" class="btn btn-primary">Sửa</a>
-                                                <a href="" class="btn btn-danger">Xóa</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>175</td>
-                                            <td>Mike Doe</td>
-                                            <td>11-7-2014</td>
-                                            <td><span class="tag tag-danger">Denied</span></td>
-                                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.
-                                            </td>
-                                            <td>
-                                                <a href="" class="btn btn-primary">Sửa</a>
-                                                <a href="" class="btn btn-danger">Xóa</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>134</td>
-                                            <td>Jim Doe</td>
-                                            <td>11-7-2014</td>
-                                            <td><span class="tag tag-success">Approved</span></td>
-                                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.
-                                            </td>
-                                            <td>
-                                                <a href="" class="btn btn-primary">Sửa</a>
-                                                <a href="" class="btn btn-danger">Xóa</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>494</td>
-                                            <td>Victoria Doe</td>
-                                            <td>11-7-2014</td>
-                                            <td><span class="tag tag-warning">Pending</span></td>
-                                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.
-                                            </td>
-                                            <td>
-                                                <a href="" class="btn btn-primary">Sửa</a>
-                                                <a href="" class="btn btn-danger">Xóa</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>832</td>
-                                            <td>Michael Doe</td>
-                                            <td>11-7-2014</td>
-                                            <td><span class="tag tag-primary">Approved</span></td>
-                                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.
-                                            </td>
-                                            <td>
-                                                <a href="" class="btn btn-primary">Sửa</a>
-                                                <a href="" class="btn btn-danger">Xóa</a>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td>982</td>
-                                            <td>Rocky Doe</td>
-                                            <td>11-7-2014</td>
-                                            <td><span class="tag tag-danger">Denied</span></td>
-                                            <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.
-                                            </td>
-                                            <td>
-                                                <a href="" class="btn btn-primary">Sửa</a>
-                                                <a href="" class="btn btn-danger">Xóa</a>
-                                            </td>
-                                        </tr>
+                                        <td>183</td>
+                                        <td>John Doe</td>
+                                        <td>
+                                            <a href="index.php?page=editcategory" class="btn btn-primary">Sửa</a>
+                                            <a href="" class="btn btn-danger">Xóa</a>
+                                        </td>
+                                        </tr>';
+
+                                        ?>
+
                                         </tbody>
                                     </table>
                                 </div>
