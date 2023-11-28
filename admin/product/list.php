df487403d021a60fae7dcf7581634dce6c462aad
HoaiTam30102003 <hoaitam30102003@gmail.com>
2023-11-28 21:42:36 +0700

    Admin - Đổi ID thành STT

--- admin/product/list.php
+++ admin/product/list.php
@@ -1,127 +1,105 @@
 <?php
-  include "components/header.php";
-  include "components/sidebar.php";
+include "components/header.php";
+include "components/sidebar.php";
 ?>
 
-<div class="wrapper">
-    <div class="content-wrapper">
-        <div class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-4">
-                        <h1 class="m-0">Sản Phẩm</h1>
-                    </div>
-                    <div class="col-sm-4">
-                        <?php
+    <div class="wrapper">
+        <div class="content-wrapper">
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-4">
+                            <h1 class="m-0">Sản Phẩm</h1>
+                        </div>
+                        <div class="col-sm-4">
+                            <?php
                             if(isset($mgs)) {
                                 echo $mgs;
                             }
-                        ?>
-                    </div>
+                            ?>
+                        </div>
 
+                    </div>
                 </div>
             </div>
-        </div>
-        <section class="content">
-            <div class="container-fluid">
-                <!-- viết code giao diện ở đây -->
-                <!-- /.row -->
-                <div class="row">
-                    <div class="col-12">
-                        <?php
-                        if (isset($_SESSION['error'])) {
-                            $message_err = $_SESSION['error'];
-                            unset($_SESSION['error']);
-                        }
-                        if (isset($_SESSION['message'])) {
-                            $message = $_SESSION['message'];
-                            unset($_SESSION['message']);
-                        }
-                        if (isset($message_err))
-                            echo '
-                                    <div class="alert alert-danger" role="alert">
-                                        ' . $message_err . '
-                                    </div>';
-                        if (isset($message))
-                            echo '
-                                    <div class="alert alert-success" role="alert">
-                                        ' . $message . '
-                                    </div>';
-                        ?>
-                    </div>
-                    <div class="col-12">
-                        <div class="card">
-                            <div class="card-header">
-                                <h3 class="card-title">Danh Sách Sản Phẩm</h3>
+            <section class="content">
+                <div class="container-fluid">
+                    <!-- viết code giao diện ở đây -->
+                    <!-- /.row -->
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h3 class="card-title">Danh Sách Sản Phẩm</h3>
 
-                                <div class="card-tools">
-                                    <div class="input-group input-group-sm" style="width: 150px;">
-                                        <div class="input-group-append">
-                                            <a href="index.php?page=addpro" class="btn btn-primary">
-                                                Thêm Mới
-                                            </a>
+                                    <div class="card-tools">
+                                        <div class="input-group input-group-sm" style="width: 150px;">
+                                            <div class="input-group-append">
+                                                <a href="index.php?page=addpro" class="btn btn-primary">
+                                                    Thêm Mới
+                                                </a>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <!-- /.card-header -->
-                            <div class="card-body table-responsive p-0" style="height: 470px;">
-                                <table class="table table-head-fixed text-nowrap">
-                                    <thead>
+                                <!-- /.card-header -->
+                                <div class="card-body table-responsive p-0" style="height: 470px;">
+                                    <table class="table table-head-fixed text-nowrap">
+                                        <thead>
                                         <tr>
-                                            <th>ID</th>
+                                            <th>STT</th>
                                             <th>Tên Sản Phẩm</th>
                                             <th>Giá Sản Phẩm</th>
                                             <th>Hình Ảnh</th>
                                             <th></th>
                                         </tr>
-                                    </thead>
-                                    <tbody>
+                                        </thead>
+                                        <tbody>
                                         <?php
-                                            $db = new product();
-                                            $add = $db->getList();
-                                            $i = 1;
-                                            foreach($add as $list) {
-                                                extract($list);
-                                                // var_dump($list);
-                                                echo '<tr>
+                                        $db = new product();
+                                        $add = $db->getList();
+                                        $i = 1;
+                                        foreach($add as $list) {
+                                            extract($list);
+                                            // var_dump($list);
+                                            echo '<tr>
 
 
-                                                <td>'.$id.'</td>
+                                                <td>'.$i.'</td>
                                                 <td>'.$name.'</td>
                                                 <td>'.number_format($price).' đ</td>
                                                 <td>
                                                     <img src=" '.$img_path.$image.' " style="width:100px">
                                                  </td>
                                                 <td>
-                                                    <a href="?page=editpro&id=' . $id . '" class="btn btn-primary">Sửa</a>
-                                                    <a onclick="return confirm(`Bạn có chắc muốn xóa không?`);" href="index.php?page=delpro&id=' . $id . '" type="button" class="btn btn-danger">Xóa</a>
+                                                    <a href="index.php?page=edit_pro&id=' . $id . '" class="btn btn-primary">Sửa</a>
+                                                    <a onclick="return confirm(`Bạn có chắc muốn xóa không?`);" href="index.php?page=del_pro&id=' . $id . '" type="button" class="btn btn-danger">Xóa</a>
                                                 </td>
                                             </tr>';
                                             $i++;
-                                            }
+                                        }
                                         ?>
-                                        
-                                        
-                                        
-                                    </tbody>
-                                </table>
+
+
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <!-- /.card-body -->
                             </div>
-                            <!-- /.card-body -->
+                            <!-- /.card -->
                         </div>
-                        <!-- /.card -->
                     </div>
+                    <!-- /.row -->
                 </div>
-                <!-- /.row -->
-            </div>
-        </section>
+            </section>
+        </div>
+
+
+        <aside class="control-sidebar control-sidebar-dark">
+
+        </aside>
+
+
     </div>
-
-
-    <aside class="control-sidebar control-sidebar-dark">
-
-    </aside>
-
-
-</div>
 <?php include 'components/footer.php' ?>
